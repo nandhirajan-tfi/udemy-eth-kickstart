835470f3b8a6aa9de7a763e28563ac38098d12e7
Nandhirajan Pandiaraj <nandy@trans-fi.xyz>
2022-08-14 17:08:24 +0530

    Accessing Campaign Details to view

--- pages/campaign/show.js
+++ pages/campaign/show.js
@@ -1,14 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 import Layout from "../../components/Layout";
+import Campaign from "../../ethereum/campaign";
 
 const CampaignShow = props => {
+
+    const [summary, setSummary] = useState({});
     const router = useRouter();
 
     useEffect(() => {
-        console.log(router.query.address);
+        getCampaignSummary();
+        console.log("Summary ", summary);
     }, []);
+
+    const getCampaignSummary = async () => {
+        const campaign = Campaign(router.query.address);
+        const summary = await campaign.methods.getSummary().call();
+        console.log("Sumary ", summary);
+        setSummary(summary);
+    }
 
     return (
         <Layout>
