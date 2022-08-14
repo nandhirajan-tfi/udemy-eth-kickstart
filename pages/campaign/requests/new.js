e443c3266d39ecb465b61eea96a29557fca9f99b
Nandhirajan Pandiaraj <nandy@trans-fi.xyz>
2022-08-14 19:07:02 +0530

    Create mock request form

--- pages/campaign/requests/new.js
+++ pages/campaign/requests/new.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button, Message, Input } from "semantic-ui-react";
+
+import Campaign from "../../../ethereum/campaign";
+import web3 from "../../../ethereum/web3";
+
+import { Link, Router } from "../../../routes";
+import Layout from "../../../components/Layout";
 
 const RequestNew = props => {
+    const [value, setValue] = useState(0);
+    const [description, setDescription] = useState("");
+    const [recipient, setRecipient] = useState("");
+
     return (
-        <h3> Request New </h3>
+        <Layout>
+            <h3>Create a Request</h3>
+            <Form>
+                <Form.Field>
+                    <label>Description</label>
+                    <Input
+                        value={description}
+                        onChange={(event) => setDescription(event.target.value)}
+                    />
+                </Form.Field>
+
+                <Form.Field>
+                    <label>Value in Ether</label>
+                    <Input
+                        value={value}
+                        onChange={(event) => setValue(event.target.value)}
+                    />
+                </Form.Field>
+
+                <Form.Field>
+                    <label>Recipient</label>
+                    <Input
+                        value={value}
+                        onChange={(event) => setRecipient(event.target.value)}
+                    />
+                </Form.Field>
+
+                <Button primary>Create</Button>
+            </Form>
+        </Layout>
     )
+}
+
+RequestNew.getInitialProps = async (context) => {
+    const address = context.query.address;
+
+    return {
+        address
+    };
 }
 
 export default RequestNew;
