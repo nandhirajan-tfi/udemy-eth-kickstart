2b8250a9cfc7ef8a90272f49334b6beb4173ab13
Nandhirajan Pandiaraj <nandy@trans-fi.xyz>
2022-08-14 17:00:07 +0530

    Hook useRouter to access path params from the server route

--- pages/campaign/show.js
+++ pages/campaign/show.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import Layout from "../../components/Layout";
 
 const CampaignShow = props => {
+    const router = useRouter();
+
+    useEffect(() => {
+        console.log(router.query.address);
+    }, []);
+
     return (
-        <h1>CampaignShow</h1>
+        <Layout>
+            <h1>CampaignShow</h1>
+        </Layout>
     )
 }
+
 
 export default CampaignShow;
