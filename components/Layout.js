aaced74582e72f4f1f3725082f91ea703eb19647
Nandhirajan Pandiaraj <nandy@trans-fi.xyz>
2022-08-14 15:41:39 +0530

    Wrapping Layout with Container

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { Container } from "semantic-ui-react";
 
 import Header from "./Header";
 
 const Layout = props => {
     return (
-        <div>
+        <Container>
             <Header />
             {props.children}
             <h1> I am a footer </h1>
-        </div>
+        </Container>
     )
 }
 
