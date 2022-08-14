56d4076e4f4380bcf3e58a7530562cf2a21a28b9
Nandhirajan Pandiaraj <nandy@trans-fi.xyz>
2022-08-14 17:54:33 +0530

    Replacing useEffect with getInitialProps

--- pages/campaign/show.js
+++ pages/campaign/show.js
@@ -1,32 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import React from "react";
 
 import Layout from "../../components/Layout";
 import Campaign from "../../ethereum/campaign";
+import { Card } from "semantic-ui-react";
 
 const CampaignShow = props => {
+    const { summary } = props;
 
-    const [summary, setSummary] = useState({});
-    const router = useRouter();
-
-    useEffect(() => {
-        getCampaignSummary();
-        console.log("Summary ", summary);
-    }, []);
-
-    const getCampaignSummary = async () => {
-        const campaign = Campaign(router.query.address);
-        const summary = await campaign.methods.getSummary().call();
-        console.log("Sumary ", summary);
-        setSummary(summary);
+    const renderCards = () => {
+        const items = [
+            {
+                header: props.summary.manager,
+                meta: "Address Of manager",
+                description: "The manager created this campaign"
+            }];
+        return <Card.Group items={items} />
     }
 
     return (
         <Layout>
             <h1>CampaignShow</h1>
+            {renderCards()}
         </Layout>
     )
 }
 
+CampaignShow.getInitialProps = async (context) => {
+    const campaign = Campaign(context.query.address);
+    const summary = await campaign.methods.getSummary().call();
+
+    return {
+        summary: {
+            minimumContribution: summary[0],
+            balance: summary[1],
+            requestsCount: summary[2],
+            approversCount: summary[3],
+            manager: summary[4]
+        }
+    };
+}
 
 export default CampaignShow;
