91f1e647432c3fc5da5d36496e1476f70c66773a
Nandhirajan Pandiaraj <nandy@trans-fi.xyz>
2022-08-14 20:23:06 +0530

    Adding final output screens

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,24 +1,25 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const Web3 = require("web3");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
 
-const compiledFactory = require("./build/CampaignFactory.json");
-const { WALLET_MNM, WALLET_PROVIDER_URL } = process.env;
-const provider = new HDWalletProvider(WALLET_MNM, WALLET_PROVIDER_URL);
-
+const provider = new HDWalletProvider(
+    process.env.WALLET_MNM,
+    process.env.WALLET_PROVIDER_URL
+);
 const web3 = new Web3(provider);
 
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
-    console.log("Attempting to deploy from account ", accounts[0]);
 
-    // Below lines will take few seconds to deploy
-    const result = await new web3.eth
-        .Contract(JSON.parse(compiledFactory.interface))
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(
+        JSON.parse(compiledFactory.interface)
+    )
         .deploy({ data: compiledFactory.bytecode })
-        .send({ from: accounts[0], gas: "1000000" });
+        .send({ gas: '1000000', from: accounts[0] });
 
-    console.log("Contract deployed to ", result.options.address, " This is our Contract Address");
+    console.log('Contract deployed to', result.options.address);
     provider.engine.stop();
-}
-
-module.exports = deploy;
+};
+deploy();
